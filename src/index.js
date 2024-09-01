2129faf521780b0c6141bfac77ac5cf0286dd68f
pooYAYooq <pooyayooq@gmail.com>
2024-09-02 01:28:56 +1000

    implement props

--- src/index.js
+++ src/index.js
@@ -3,25 +3,26 @@ import { createRoot } from 'react-dom/client';
 
 import './index.css';
 
+const title = 'Atomic Habits';
+const author = 'James Clear';
+const img = './images/book-1.jpg';
+
+
 function BookList() {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
+      <Book author={author} title={title} img={img} />
       <Book />
     </section>
   );
 }
 
-const author = 'James Clear';
-const Book = () => {
-  const title = 'Atomic Habits';
+const Book = (props) => {
   return (
     <article className="book">
-      <img src="./images/book-1.jpg" alt="Book Cover" />
-      <h2>{title}</h2>
-      <h4>{author.toUpperCase()}</h4>
+      <img src={props.img} alt={props.title} />
+      <h2>{props.title}</h2>
+      <h4>{props.author}</h4>
     </article>
   );
 };
