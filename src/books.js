33306864b64f6e0e4073be7621eb475998ee0e5e
pooYAYooq <pooyayooq@gmail.com>
2024-09-05 03:59:03 +1000

    implement ES6 modules

---
+++ src/books.js
@@ -0,0 +1,22 @@
+const books = [
+  {
+    author: 'James Clear',
+    title: 'Atomic Habits',
+    img: './images/book-1.jpg',
+    id: 1,
+  },
+  {
+    author: 'Colleen Hoover',
+    title: 'It Ends With Us',
+    img: './images/book-2.jpg',
+    id: 2,
+  },
+  {
+    author: 'Lauren Roberts',
+    title: 'Reckless',
+    img: './images/book-3.jpg',
+    id: 3,
+  },
+];
+
+export default books;
--- src/index.js
+++ src/index.js
@@ -2,27 +2,8 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 
 import './index.css';
-
-const books = [
-  {
-    author: 'James Clear',
-    title: 'Atomic Habits',
-    img: './images/book-1.jpg',
-    id: 1,
-  },
-  {
-    author: 'Colleen Hoover',
-    title: 'It Ends With Us',
-    img: './images/book-2.jpg',
-    id: 2,
-  },
-  {
-    author: 'Lauren Roberts',
-    title: 'Reckless',
-    img: './images/book-3.jpg',
-    id: 3,
-  },
-];
+import books from './books';
+import Book from './Book';
 
 function Booklist() {
   return (
@@ -34,18 +15,8 @@ function Booklist() {
   );
 }
 
-const Book = ({ img, title, author }) => {
-  return (
-    <article className="book">
-      <img src={img} alt={title} />
-      <h2>{title}</h2>
-      <h4>{author}</h4>
-    </article>
-  );
-};
+
 
 const root = createRoot(document.getElementById('root'));
 
 root.render(<Booklist />);
-
-
