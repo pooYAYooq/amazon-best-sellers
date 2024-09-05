59a32ae305283d9fea534350f7636c4b982c2e0e
pooYAYooq <pooyayooq@gmail.com>
2024-09-05 17:19:55 +1000

    move images to src folder and import them to books.js

--- src/books.js
+++ src/books.js
@@ -1,20 +1,24 @@
+import img1 from './images/book-1.jpg';
+import img2 from './images/book-2.jpg';
+import img3 from './images/book-3.jpg';
+
 const books = [
   {
     author: 'James Clear',
     title: 'Atomic Habits',
-    img: './images/book-1.jpg',
+    img: img1,
     id: 1,
   },
   {
     author: 'Colleen Hoover',
     title: 'It Ends With Us',
-    img: './images/book-2.jpg',
+    img: img2,
     id: 2,
   },
   {
     author: 'Lauren Roberts',
     title: 'Reckless',
-    img: './images/book-3.jpg',
+    img: img3,
     id: 3,
   },
 ];
